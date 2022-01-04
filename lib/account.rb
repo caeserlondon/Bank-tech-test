32cc5acb272df2d53ea3beb18fd5a08f198edd7b
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-01-04 16:27:01 +0000

    User Story 3 adding and testing withdraw method

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
+# the main class
 class Account
-  attr_reader :balance
+  attr_reader :balance, :transactions_log
 
   def initialize
     @balance = 0
+    @transactions_log = []
   end
 
   def deposite(amount)
     @balance += amount
+    transactions_log.push(amount)
+  end
+
+  def withdraw(amount)
+    raise 'Error! Insufficient funds' if (@balance - amount).negative?
+    @balance -= amount
+    transactions_log.push(-amount)
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,8 +3,23 @@
 require 'account'
 
 describe Account do
-  it 'Adds the deposite value to the balance' do
-    account = Account.new
-    expect { account.deposite(1) }.to change { account.balance }.by(1)
+  describe 'deposit' do
+    it 'Adds the deposite value to the balance' do
+      account = Account.new
+      expect { account.deposite(1) }.to change { account.balance }.by(1)
+    end
+
+    it 'Record the transactions in the transactions log' do
+      account = Account.new
+      expect { account.deposite(1) }.to change { account.transactions_log.size }.by(1)
+    end
+  end
+
+  describe 'withdraw' do
+    it 'Should subtract the withdrawal amount from the balance' do
+      account = Account.new
+      account.deposite(1)
+      expect { account.withdraw(1) }.to change { account.balance }.by(-1)
+    end
   end
 end
