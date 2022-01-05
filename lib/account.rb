8989aadc5f2ccfeafe869b3a3edb566184403102
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-01-05 17:02:41 +0000

    adding real time stamp to transactions

--- lib/account.rb
+++ lib/account.rb
@@ -9,29 +9,31 @@ class Account
     @balance = 0
   end
 
-  def deposit(credit, date)
-    raise 'Transaction not allowed! Please select another amount to deposit.' if credit.zero?
-    raise 'Transaction not allowed! Please select a positive amount to deposit.' if credit.negative?
+  def deposit(credit)
+    raise 'Please select another amount to deposit.' if credit.zero?
+    raise 'Please select another amount to deposit.' if credit.negative?
 
+    @date = Time.now.strftime('%d/%m/%Y')
     @balance += credit
-    @transactions << [date, credit, '', balance]
+    @transactions << [@date, credit, '', balance]
     'transaction completed successfully'
   end
 
-  def withdraw(debit, date)
+  def withdraw(debit)
     raise 'Transaction not allowed! Insufficient funds!' if @balance < debit
     raise 'Transaction not allowed! Please select an amount to withdraw.' if debit.zero?
     raise 'Transaction not allowed! Only positive amounts can be withdrawn.' if debit.negative?
 
+    @date = Time.now.strftime('%d/%m/%Y')
     @balance -= debit
-    @transactions << [date, '', debit, balance]
+    @transactions << [@date, '', debit, balance]
     'transaction completed successfully'
   end
 
   def statement_printer
-    puts 'date || credit || debit || balance'
+    puts 'date || credit || debit || balance '
     @transactions.map do |tr|
-      puts "#{tr[0]} || #{tr[1]} || #{tr[2]} || #{tr[3]}"
+      puts " #{tr[0]} || #{tr[1]} || #{tr[2]} || #{tr[3]}"
     end
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,59 +1,55 @@
 # frozen_string_literal: true
 
 require 'account'
+require 'time'
 
-describe Account do
+describe 'Account' do
   let(:account) { Account.new }
+  let(:date) { Time.now.strftime('%d/%m/%Y') }
 
   describe '#deposit' do
     it 'accepts credit transaction' do
-      account.deposit(10, '10-01-2023')
-      expect(account.transactions).to include(['10-01-2023', 10, '', 10])
-    end
-
-    it 'Adds the deposit value to the balance' do
-      expect { account.deposit(10, '10-01-2023') }.to change { account.balance }.by(10)
+      account.deposit(10)
+      expect(account.balance).to eq(10)
     end
 
     it 'raises an error if the deposit is zero' do
-      expect { account.deposit(0, '10-01-2023') }.to raise_error('Transaction not allowed! Please select another amount to deposit.')
+      expect { account.deposit(0) }.to raise_error('Please select another amount to deposit.')
     end
+
     it 'raises an error if the deposit is a negative amount' do
-      expect do
-        account.deposit(-10,
-                        '10-01-2023')
-      end.to raise_error('Transaction not allowed! Please select a positive amount to deposit.')
+      expect { account.deposit(-10) }.to raise_error('Please select another amount to deposit.')
     end
   end
 
   describe '#withdraw' do
     it 'accepts debit transaction' do
-      account.deposit(10, '10-01-2023')
-      account.withdraw(10, '14-01-2023')
-      expect(account.transactions).to include(['14-01-2023', '', 10, 0])
+      account.deposit(10)
+      account.withdraw(10)
+      expect(account.balance).to eq(0)
     end
 
     it 'Should subtract the withdraw ammount from the balance' do
-      account.deposit(10, '10-01-2023')
-
-      expect { account.withdraw(10, '14-01-2023') }.to change { account.balance }.by(-10)
+      account.deposit(10)
+      expect { account.withdraw(10) }.to change { account.balance }.by(-10)
     end
 
     it 'Raises an error if Insufficient funds' do
-      expect { account.withdraw(10, '14-01-2023') }.to raise_error('Transaction not allowed! Insufficient funds!')
+      expect { account.withdraw(10) }.to raise_error('Transaction not allowed! Insufficient funds!')
     end
 
     it 'raises an error if withdrawing zero' do
-      expect { account.withdraw(0, '14-01-2023') }.to raise_error('Transaction not allowed! Please select an amount to withdraw.')
+      expect { account.withdraw(0) }.to raise_error('Transaction not allowed! Please select an amount to withdraw.')
     end
   end
 
   describe '#statement_printer' do
     it 'prints the user statement' do
-      account.deposit(10, '10-01-2023')
+      account.deposit(10)
+      @date = Time.now.strftime('%d/%m/%Y')
       expect do
         account.statement_printer
-      end.to output("date || credit || debit || balance\n10-01-2023 || 10 ||  || 10\n").to_stdout
+      end.to output("date || credit || debit || balance \n #{@date} || 10 ||  || 10\n").to_stdout
     end
   end
 end
