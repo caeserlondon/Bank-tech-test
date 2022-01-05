d70e2a21f63985037da8e692debc43b10752cd9d
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-01-05 09:38:06 +0000

    adding the balance class variable

--- lib/account.rb
+++ lib/account.rb
@@ -2,17 +2,20 @@
 
 # the main class
 class Account
-  attr_reader :transactions
+  attr_reader :transactions, :balance
 
   def initialize
     @transactions = []
+    @balance = 0
   end
 
   def deposite(credite, date)
+    @balance += credite
     @transactions << [credite, date]
   end
 
   def withdraw(debit, date)
+    @balance -= debit
     @transactions << [-debit, date]
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -11,12 +11,22 @@ describe 'Account' do
       account.deposite(10, '10-01-2023')
       expect(account.transactions).to include([10, '10-01-2023'])
     end
+
+    it 'Adds the deposit value to the balance' do
+      expect { account.deposite(10, '10-01-2023') }.to change { account.balance }.by(10)
+    end
   end
 
   describe 'withdraw' do
     it 'accepts debit transaction' do
       account.withdraw(10, '14-01-2023')
       expect(account.transactions).to include([-10, '14-01-2023'])
+    end
+
+    it 'Should subtract the withdraw ammount from the balance' do
+      account.deposite(10, '10-01-2023')
+
+      expect { account.withdraw(10, '14-01-2023') }.to change { account.balance }.by(-10)
     end
   end
 end
