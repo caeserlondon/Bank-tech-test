68d80d6f1ce203de2e81f14d85c85d87ecd0fdd8
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-01-05 08:38:10 +0000

    refactoring the test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,27 +3,27 @@
 require 'account'
 
 describe Account do
+
+  let (:account) { Account.new }
+
   describe 'deposit' do
-    it 'Adds the deposite value to the balance' do
-      account = Account.new
+    it 'accepts credit transactions ' do
+      account.deposite(1)
       expect { account.deposite(1) }.to change { account.balance }.by(1)
     end
 
     it 'Record the transactions in the transactions log' do
-      account = Account.new
       expect { account.deposite(1) }.to change { account.transactions_log.size }.by(1)
     end
   end
 
   describe 'withdraw' do
     it 'Should subtract the withdrawal amount from the balance' do
-      account = Account.new
       account.deposite(1)
       expect { account.withdraw(1) }.to change { account.balance }.by(-1)
     end
 
     it 'Records the transaction to the transaction log' do
-      account = Account.new
       account.deposite(1)
       expect { account.withdraw(1) }.to change { account.transactions_log.length }.by(1)
     end
